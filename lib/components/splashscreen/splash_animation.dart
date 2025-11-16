983c7884c60b295b89ea5f5174ae93382e5a352f
darknecrocities <parejasarronkian@gmail.com>
2025-11-16 21:48:51 +0800

    feat: push

--- lib/components/splashscreen/splash_animation.dart
+++ lib/components/splashscreen/splash_animation.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class CustomPageRoute extends PageRouteBuilder {
   final Widget child;
 
+
   CustomPageRoute({required this.child})
       : super(
     transitionDuration: const Duration(milliseconds: 700),
