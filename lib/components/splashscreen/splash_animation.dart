b6fa16a4ac09476847339de478a12d2e34a15274
darknecrocities <parejasarronkian@gmail.com>
2025-11-22 15:43:36 +0800

    fix: correction on the error

--- lib/components/splashscreen/splash_animation.dart
+++ lib/components/splashscreen/splash_animation.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class CustomPageRoute extends PageRouteBuilder {
   final Widget child;
 
-
   CustomPageRoute({required this.child})
       : super(
     transitionDuration: const Duration(milliseconds: 700),
